086b32925ef04c1b8717e84df655f6f2f78c546b
leansl96 <leansl96@gmail.com>
2026-04-03 13:24:28 -0300

    modifiqued

--- app.py
+++ app.py
@@ -203,5 +203,5 @@ def detalle_producto(producto_id):
 # =========================
 # RUN
 # =========================
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
